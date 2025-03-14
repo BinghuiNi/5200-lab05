ddcf70319f924ee2fdb21fc1754346407da00fb6
Binghui Ni <bn259@georgetown.edu>
2025-03-14 18:35:34 -0400

    Reinitiate renv and create an interactive map

--- .vdoc.r
+++ .vdoc.r
@@ -64,27 +64,42 @@ wards <- geojson_sf("data/wards/ACS_Demographic_Characteristics_DC_Ward.geojson"
 #
 #
 #
-counties <- us_map("counties")
-full_map <-
-  tm_shape(counties, bbox = st_bbox(metro)) + tm_polygons(col = "white")
-full_map
+wards_map <- tm_shape(wards) + 
+  tm_polygons(col = "DP05_0001E", palette = "Blues", style = "cont", title = "Total Population") + 
+  tm_layout(main.title = "DC Population Heatmap by Ward", frame = FALSE) +
+  tm_text(text = "DP05_0001E", size = 1, col = "red", fontface = "bold")
+wards_map
+#
+#
+#
+#
+#
+rr <- tags$div(
+   HTML('<h3 align="center" style="font-size:16px"><b>DC Population Heatmap by Ward</b></h3>')
+ )
 
-wards_map <- full_map +
-  tm_shape(wards) + 
-  tm_polygons(col = "DP05_0001E", palette = "YlOrRd", style = "cont", title = "Total Population") +  # 为选区着色
-  tm_layout(main.title = "Population Heatmap by Ward", frame = FALSE)
+plt <- leaflet(wards) %>%
+  setView(lng = -77, lat = 38.9, zoom = 11) %>%
+  addProviderTiles("CartoDB.Positron") %>%
+  addPolygons(data = wards, 
+              fillColor = ~colorNumeric(palette = "Blues", domain = wards$DP05_0001E)(DP05_0001E),
+              weight = 1, opacity = 1, color = "white", fillOpacity = 0.7) %>%
+  addControl(rr, position = "bottomright") %>%
+  addLabelOnlyMarkers(data = wards,
+                    lng = ~st_coordinates(geometry)[, 1], 
+                    lat = ~st_coordinates(geometry)[, 2], 
+                    label = ~as.character(DP05_0001E), 
+                    labelOptions = labelOptions(noHide = TRUE, 
+                                                direction = "center", 
+                                                textsize = "12px", 
+                                                fontface = "bold", 
+                                                color = "red"))
 
-# 显示地图
-full_map
-
-tm_shape(wards) + 
-  tm_polygons(col = "DP05_0001E",
-              palette = "YlOrRd",
-              style = "cont",
-              title = "Total Population") +
-  tm_layout(main.title = "Population Heatmap by Ward",
-            main.title.position = c("center", "top"),
-            frame = FALSE)
+plt
+#
+#
+#
+#
 #
 #
 #
