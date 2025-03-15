187610cac3aae25590c0b4ec7c1f44926866b8db
Binghui Ni <bn259@georgetown.edu>
2025-03-15 00:33:17 -0400

    Add a map using Observable plot

--- .vdoc.r
+++ .vdoc.r
@@ -16,30 +16,6 @@
 #
 #
 #
-# Restore the existing environment from renv.lock
-renv::restore()
-#
-#
-#
-#
-# Install packages
-install.packages("tidyverse")
-install.packages("sf")
-install.packages("geojsonsf")
-install.packages("leaflet")
-install.packages("tmap")
-install.packages("spData")
-install.packages("usmap")
-install.packages("htmlwidgets")
-install.packages("htmltools")
-
-# Update the renv.lock file
-renv::snapshot()
-#
-#
-#
-#
-#
 library(tidyverse)
 library(sf) # simple features in R, enables geographic viz
 library(geojsonsf) # reading GeoJSON files into the right format
@@ -74,28 +50,9 @@ wards_map
 #
 #
 #
-rr <- tags$div(
-   HTML('<h3 align="center" style="font-size:16px"><b>DC Population Heatmap by Ward</b></h3>')
- )
-
-plt <- leaflet(wards) %>%
-  setView(lng = -77, lat = 38.9, zoom = 11) %>%
-  addProviderTiles("CartoDB.Positron") %>%
-  addPolygons(data = wards, 
-              fillColor = ~colorNumeric(palette = "Blues", domain = wards$DP05_0001E)(DP05_0001E),
-              weight = 1, opacity = 1, color = "white", fillOpacity = 0.7) %>%
-  addControl(rr, position = "bottomright") %>%
-  addLabelOnlyMarkers(data = wards,
-                    lng = ~st_coordinates(geometry)[, 1], 
-                    lat = ~st_coordinates(geometry)[, 2], 
-                    label = ~as.character(DP05_0001E), 
-                    labelOptions = labelOptions(noHide = TRUE, 
-                                                direction = "center", 
-                                                textsize = "12px", 
-                                                fontface = "bold", 
-                                                color = "red"))
-
-plt
+tmap_mode("view")
+tm_basemap(leaflet::providers$CartoDB.Positron) +
+  wards_map
 #
 #
 #
